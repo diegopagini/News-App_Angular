17bff94187d0ad82da38dbff1a4495842eb9d878
Diego Pagini <dq@qbkconsulting.com>
2021-05-20 09:28:06 -0300

    reactive form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -7,14 +7,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  public categoriesForm: FormGroup;
+  public searchForm: FormGroup;
+  public categories: any[] = [
+    { value: 'general', nombre: 'General' },
+    { value: 'business', nombre: 'Negocios' },
+    { value: 'entertainment', nombre: 'Entretenimiento' },
+    { value: 'healt', nombre: 'Salud' },
+    { value: 'science', nombre: 'Ciencia' },
+    { value: 'sports', nombre: 'Deportes' },
+    { value: 'technology', nombre: 'Tecnología' },
+  ];
+  public countries: any[] = [
+    { value: 'ar', nombre: 'Argentina' },
+    { value: 'br', nombre: 'Brasil' },
+    { value: 'fr', nombre: 'Francia' },
+    { value: 'hu', nombre: 'Hungria' },
+    { value: 'mx', nombre: 'Mexico' },
+    { value: 'gb', nombre: 'Reino Unido' },
+  ];
 
   constructor(private fb: FormBuilder) {
-    this.categoriesForm = this.fb.group({
+    this.searchForm = this.fb.group({
       categories: ['', [Validators.required]],
       country: ['', [Validators.required]],
     });
   }
 
   ngOnInit(): void {}
+
+  sendRequest() {
+    console.log(this.searchForm.value);
+  }
 }
